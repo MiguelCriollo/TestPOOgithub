591fc8649c84624e35e75c54c00cb3134dad3f7a
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-07-22 10:47:27 -0500

    Part1????

--- PagesBase/main.js
+++ PagesBase/main.js
@@ -1,4 +1,4 @@
-import { objetoTest } from "../TESTS/MostradoParrafos/objetos.js";
+import {text} from "./texto.js";
 
 let historyText = document.getElementById("historyText"); //Parrafo de la historia
 let nextButton=document.getElementById("button-next"); //Boton de Continuar
@@ -6,15 +6,32 @@ let nextButton=document.getElementById("button-next"); //Boton de Continuar
 let contenedorParrafo=document.getElementById("historyContainer");
 let alturaInicial=contenedorParrafo.offsetHeight;
 
-console.log(`Tama;o contenedor parrafo : ${alturaInicial}`);
+console.log(text);
 
 nextButton.addEventListener("click",activatePrinting);
     
 function activatePrinting(){
+    let contadorsito=0;
+
+    historyText.textContent="";
+    let cont=0;
+    let intervalo;
+    let textToPrint=text.initial[contadorsito];
+    
+    function test(){
+        historyText.textContent+=textToPrint[cont];
+        cont++;
+        textToPrint[cont]??clearInterval(intervalo);
+    }
+    intervalo=setInterval(test, 30);
+    contadorsito++;
+}
+/*
+function activatePrinting(){
         historyText.textContent="";
         let cont=0;
         let intervalo;
-        let textToPrint="SAMPLE TEXT  ".repeat(700);
+        let textToPrint="SAMPLE TEXT  ".repeat(200);
         
         function test(){
             historyText.textContent+=textToPrint[cont];
@@ -30,7 +47,7 @@ function activatePrinting(){
         }
         intervalo=setInterval(test, 1);
 }
-
+/*
 /****BOTONES******/
 
 
---
+++ PagesBase/texto.js
@@ -0,0 +1,23 @@
+ export const text={
+    initial: [
+        'Con cada paso que das es una agonía de sufrimiento, miras alrededor en busca de ayuda pero lo que vez causa más desesperación en tu ser, vez como un padre entra en una casa en llamas con el propósito de salvar a su familia, la casa colapsó en ese instante.',
+        'Miras como el religioso ora con todas sus fuerzas por ayuda divina, solo para darte cuenta que una viga se aloja en su vientre atrapandolo, nadie podrá ayudarlo.',
+        'Tus pasos se detienen, no por ti, si no por una mano que te sujeta con desesperación, al mirar a la dueña de esta mano, la esperanza comenzó a surgir en los ojos de aquella chica, pero tu no ves eso, en lo que te concentras es como un muro de concreto está aplastando los pies de la chica.',
+        'Es imposible salvarla, lo sabes, no hay nada en lo que puedas ayudarla, entonces haces lo único que puedes hacer, sigues caminando ignorando las súplicas de salvación que la chica grita desgarrando su garganta por el smog en el aire por la carne quemada, con cada paso que das sus súplicas se convierten en gritos, y los gritos en maldiciones, ella te desea la muerte por dejarla abandonada.',
+        'Tus pensamientos se detienen, nada bueno sale recordando, por eso sigues caminando por este lugar, un lugar donde no hay algún sentido del tiempo, donde tu existencia no es más que una anormalidad, un sitio donde solo hay un infinito sin nada, no sabes el porque caminas, no tienes ambiciones.',
+        'Lo que alguna vez fuiste ya no existe para utilizarlo como motor, aun así dudas en seguir caminando sabiendo que sería inutil, sin embargo sigues caminando ya que es lo único que puedes hacer, porque sabes que debe haber algo más allá de toda la nada que te rodea.'
+      ],
+    D1: {
+        text: `Seguir avanzando`,
+        postD:`Al seguir adelante, sin necesidad de esperar alguna ayuda, es el primer paso para encontrar tu salvación, o la condena.`
+    },
+    
+    D2: {
+        text: `Quedarse`,
+        postD:`Sigues esperando un rumbo al que ir por toda la eternidad.`
+    },
+    post:`Al dejar de lado las dudas que atormentan tu mente sigues caminando, aunque tú mismo sabes que es falso, siempre tendrás dudas pero eso no quiere decir que no puedes seguir avanzando por este camino que has elegido recorrer, la pregunta será, ¿podrás llegar hasta el final?`
+
+}
+
+
