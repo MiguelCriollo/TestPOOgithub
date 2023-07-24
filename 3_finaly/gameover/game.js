ff6ae834f94fd569de02eb480de6f7b1a92a76b4
Franco Capelo <115761575+FrancoCapelo29@users.noreply.github.com>
2023-07-24 08:25:44 -0500

    2

---
+++ 3_finaly/gameover/game.js
@@ -0,0 +1,8 @@
+function redireccion() {
+    window.location.href = "/levels/level-0/history/history.html";
+  }
+  document.addEventListener("DOMContentLoaded", function () {
+    var audioPlayer = document.getElementById("audio-player");
+    audioPlayer.pause();
+  });
+  
