a4ddff225789df4d3cc7a66f134a1dbeb16c5894
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-07-25 10:09:31 -0500

    Update inicio.js

--- inicio.js
+++ inicio.js
@@ -1,7 +1,7 @@
 confirm("¿Estás listo para adentrarte en la oscuridad y enfrentar tu destino?");
 
 function redireccion() {
-    window.location.href = "/levels/level-0/history/history.html";
+    window.location.href = "/levels/level-0/prologo.html";
 }
 
 document.addEventListener("DOMContentLoaded", function () {
