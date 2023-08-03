c1c74d4d9205c697647f9e53775475c0a461b5fc
Miguell Criollo <macriollo.2@sudamericano.edu.ec>
2023-08-03 05:08:42 -0500

    Final Integration

---
+++ mainChoices/text.js
@@ -0,0 +1,108 @@
+ export const text={
+    lvl:[
+        {
+            initial: [
+            `Con cada paso que das es una agonía de sufrimiento, miras alrededor en busca de ayuda pero lo que vez causa más desesperación en tu ser, vez como un padre entra en una casa en llamas con el propósito de salvar a su familia, la casa colapsó en ese instante.
+            Miras como el religioso ora con todas sus fuerzas por ayuda divina, solo para darte cuenta que una viga se aloja en su vientre atrapandolo, nadie podrá ayudarlo.`,
+            `Tus pasos se detienen, no por ti, si no por una mano que te sujeta con desesperación, al mirar a la dueña de esta mano, la esperanza comenzó a surgir en los ojos de aquella chica, pero tu no ves eso, en lo que te concentras es como un muro de concreto está aplastando los pies de la chica.
+            Es imposible salvarla, lo sabes, no hay nada en lo que puedas ayudarla, entonces haces lo único que puedes hacer, sigues caminando ignorando las súplicas de salvación que la chica grita desgarrando su garganta por el smog en el aire por la carne quemada, con cada paso que das sus súplicas se convierten en gritos, y los gritos en maldiciones, ella te desea la muerte por dejarla abandonada.`,
+            `Tus pensamientos se detienen, nada bueno sale recordando, por eso sigues caminando por este lugar, un lugar donde no hay algún sentido del tiempo, donde tu existencia no es más que una anormalidad, un sitio donde solo hay un infinito sin nada, no sabes el porque caminas, no tienes ambiciones.
+            Lo que alguna vez fuiste ya no existe para utilizarlo como motor, aun así dudas en seguir caminando sabiendo que sería inutil, sin embargo sigues caminando ya que es lo único que puedes hacer, porque sabes que debe haber algo más allá de toda la nada que te rodea.`
+            ],
+            optionA: {
+                text: `Seguir avanzando`,
+                postD:`Avanzar es lo unico que puedes hacer, incluso si te quedas aqui, no habria nada que hacer mas que solo lamentar tu existencia, entonces con el dolor en tu alma, con el ardor en tu cuerpo desgastado, sigues adelante abandonando tus dudas.`
+            },
+            
+            optionB: {
+                text: `Rendirse`,
+                postD:`Una existencia vacia sin un proposito, eso es lo que eres, no importa cuanto quieras negarlo, entonces solo te queda estar varado en este lugar donde la nada sera tu unico acompañante, en verdad que desperdicio de existencia`
+            },
+            finalText:`Aunque tú mismo sabes que es falso, siempre tendrás dudas sobre tu existencia, dudas del porque sigues aqui mientras que otros no, pero eso no quiere decir que no puedas seguir avanzando por este camino que has elegido recorrer, la pregunta será, ¿podrás llegar hasta el final?`
+        },
+        {
+            initial: [
+                `A pesar de ello, los ignoras, nada bueno sale dejando te influenciar por aquellos que se resignaron a un destino sin sentido, ¿O deberías hacerlo?.
+                Sin saber por dónde ir, te acercas a una de aquellas personas y preguntas por indicaciones, estas personas a pesar de no haber avanzado, saben muy bien como hacerlo, pero temen intentarlo, dando las gracias por las indicaciones, sigues con tu camino hacia un lago que está en la parte izquierda de esta caverna.`,
+                `El tan solo acercarte aquel lago provoca un sentimiento de opresión sobre tu pecho, como si acercarte fuera una prueba hercúlea en sí mismo, lo ignoras todo con tal de seguir adelante, pero eso fue tu error, porque el tan solo haber dado un pie en el agua fue suficiente para que estas te arrastrarán a las profundidades buscando ahogarte.`
+              ],
+            optionA: {
+                text: `Luchar`,
+                postD:`No te permitiras morir cuando tan solo has dado el primer paso, sabes que no estas aqui para ser cuidado, no despues de haber abandonado a tanta gente con tal de salvarte.
+                Logras agarrarte de una roca, lo suficientemente grande para comenzar arrastrarte a la superficie, sabes que no puedes esperar a que alguien te rescate y no debes hacerlo, logras salir a la superficie.`
+            },
+            
+            optionB: {
+                text: `Rendirse`,
+                postD:`Una existencia vacia sin un proposito, eso es lo que eres, no importa cuanto quieras negarlo, entonces solo te queda estar varado en este lugar donde la nada sera tu unico acompañante, en verdad que desperdicio de existencia`
+            },
+            finalText:`Salir de aquel lago no fue una cosa sencilla de realizar, pero tus ganas de vivir fueron lo suficientemente fuertes como para poder hacerlo, el sentir el viento en tu piel hace que te relajes y logres respirar con tranquilidad, pasando tus manos por el cuello logras sentir algo que no habia estado ahi antes, al sujetar dicho objeto notas que es una cruz hecho de madera simple atado con un cordón alrededor de tu cuello.
+            Dejando de lado aquel “crucifijo” comienzas a caminar hacia la salida de aquel sitio, una puerta ornamentada de forma sencilla, a medida que te vas acercando ves como la puerta anteriormente cerrada comienza abrirse, invitandote a seguir adelante.`
+        
+        },
+        {
+            initial: [
+                `No comprendes como una tormenta puede existir aquí abajo donde las corrientes de aire no tocan este sitio perdido, pero aun así existe, y ves el dolor que provoca a diferentes personas que están en su interior.
+                Personas incapaces de salvarse a sí mismos de aquel castigo, pues como una persona puede ir contra la marea cuando está atrapada en ella, como un cordero puede regresar cuando no sabe qué dirección tomar, incluso a veces prefieren esto que a regresar donde antes, pues como se puede explicar que las mismas personas que están encerradas ahí dentro, siguen desenfrenadas por sus deseos más primarios.`,
+                `Están perdidos, no hay salvación para ellos, porque incluso mientras sus cuerpos están siendo desgarrados, su piel desprendiéndose de la carne, él como partes de su cuerpo están en direcciones equivocadas , ellos disfrutan de su lujuria, se regocijan de ella, sin saber que nunca escaparan de este infierno.`
+              ],
+            optionA: {
+                text: `Caer`,
+                postD:`A pesar de reflexionar sobre lo que has visto, una parte de tu alma aún anhelante de afecto físico, decide que a pesar de sufrir de aquel castigo, después de todo, estar solo es un castigo aún peor.
+                Tu alma ha ingresado a un círculo vicioso donde no tendrás escapatoria, incluso si conseguiste aquel calor que estabas anhelando, tu alma quedará condenada a este sufrimiento eterno.`
+            },
+            
+            optionB: {
+                text: `Rechazar`,
+                postD:`Sujetando con fuerza aquel crucifijo que cuelga de tu cuello, té armas de valor para seguir adelante, usándolo como un amuleto para calmar tus deseos más primarios que quieren resurgir de tu alma.
+                Incluso con los fuertes vientos, sigues tu camino hasta llegar a la siguiente sección, al mirar hacia atrás lo único que puedes hacer es lamentarte por aquellas personas, sin darte cuenta de cómo la pequeña cruz brilla con luz calmante.`
+            },
+            finalText:`Al abandonar cualquier tipo de deseo carnal, sigues tu camino atravesando aquellos fuertes vientos que azotan tu carne suave, sientes como la carne quiere desprenderse de tu cuerpo, pero sigues adelante ignorando cualquier tipo de sonido, tus ojos arden cuanto más avanzas por el viento que aumentó la fuerza de sus corrientes, aun asi sigues ignorando cualquier tipo de dolor.
+            No sabes cuanto tiempo has estado así caminando por este tormento, pero no te detienes, hacerlo significa sufrir esto para siempre, por lo tanto sigues adelante, de forma abrupta la tormenta comienza a cesar, el viento que antes podía desgarrar la carne ahora es apenas una simple brisa, mirando al frente notas como la salida de este lugar está cerca.`
+        },
+        {
+            initial: [
+                `No comprendes como una tormenta puede existir aquí abajo donde las corrientes de aire no tocan este sitio perdido, pero aun así existe, y ves el dolor que provoca a diferentes personas que están en su interior.
+                Personas incapaces de salvarse a sí mismos de aquel castigo, pues como una persona puede ir contra la marea cuando está atrapada en ella, como un cordero puede regresar cuando no sabe qué dirección tomar, incluso a veces prefieren esto que a regresar donde antes, pues como se puede explicar que las mismas personas que están encerradas ahí dentro, siguen desenfrenadas por sus deseos más primarios.`,
+                `Están perdidos, no hay salvación para ellos, porque incluso mientras sus cuerpos están siendo desgarrados, su piel desprendiéndose de la carne, él como partes de su cuerpo están en direcciones equivocadas , ellos disfrutan de su lujuria, se regocijan de ella, sin saber que nunca escaparan de este infierno.`
+              ],
+            optionA: {
+                text: `Caer`,
+                postD:`A pesar de reflexionar sobre lo que has visto, una parte de tu alma aún anhelante de afecto físico, decide que a pesar de sufrir de aquel castigo, después de todo, estar solo es un castigo aún peor.
+                Tu alma ha ingresado a un círculo vicioso donde no tendrás escapatoria, incluso si conseguiste aquel calor que estabas anhelando, tu alma quedará condenada a este sufrimiento eterno.`
+            },
+            
+            optionB: {
+                text: `Rechazar`,
+                postD:`Sujetando con fuerza aquel crucifijo que cuelga de tu cuello, té armas de valor para seguir adelante, usándolo como un amuleto para calmar tus deseos más primarios que quieren resurgir de tu alma.
+                Incluso con los fuertes vientos, sigues tu camino hasta llegar a la siguiente sección, al mirar hacia atrás lo único que puedes hacer es lamentarte por aquellas personas, sin darte cuenta de cómo la pequeña cruz brilla con luz calmante.`
+            },
+            finalText:`Al abandonar cualquier tipo de deseo carnal, sigues tu camino atravesando aquellos fuertes vientos que azotan tu carne suave, sientes como la carne quiere desprenderse de tu cuerpo, pero sigues adelante ignorando cualquier tipo de sonido, tus ojos arden cuanto más avanzas por el viento que aumentó la fuerza de sus corrientes, aun asi sigues ignorando cualquier tipo de dolor.
+            No sabes cuanto tiempo has estado así caminando por este tormento, pero no te detienes, hacerlo significa sufrir esto para siempre, por lo tanto sigues adelante, de forma abrupta la tormenta comienza a cesar, el viento que antes podía desgarrar la carne ahora es apenas una simple brisa, mirando al frente notas como la salida de este lugar está cerca.`
+        },
+        {
+            initial: [
+                `No sabes con que te toparás si sigues, pero cualquier cosa es mejor.
+                Una luz cegó tu visión cuando notaste la salida del final de este túnel, algo que te llama la atención, ¿cómo podría el sol brillar desde este sitio?, la respuesta de aquella pregunta se te es respondida sin que nadie te lo diga, la razón es que no hay sol aquí abajo, no, lo que hay es solo riquezas sin fin, desde donde estas, solo puedes observar montañas de oro.`,
+                `Montañas que se extienden por toda la existencia de esta recamara, desde figuras decorativas, el oro, la plata e incluso diamantes yace ante ti, como una recompensa por haber llegado tan lejos, aunque no sea así, tu mente está tratando de deformar la respuesta con tal de quedarte con estas riquezas, cuando sabes que no has hecho nada más que caminar para escapar.`,
+                `Hay riquezas que muchos Reyes habrían matado por poseer, pero tu solo los admiras como algo brillante, ¿como puede brillar algo tan intensamente en este infierno?, llevado por tu curiosidad alzas una baratija casi sin importancia, es una vara larga, su punta es filosa, un arma ornamentada de tal forma mas exquisita que existe, en muchos sentidos la lanza que ahora tienes en tus manos es un arma sin igual, arma que muchos daria sus brazos a torcer con tal de poseerla.`,
+                `Un arma que para ti no significa nada mas que solo una herramienta.`
+              ],
+            optionA: {
+                text: `Llevar`,
+                postD:`La codicia es algo que no logras comprender, tu alma no tiene ese sentido ya que terminó por ser abandonado, lo que sí sabes es que necesitas algo para defenderte, hasta ahora no te has topado con grandes desafíos, solo con reflexiones, decides llevarte la lanza como una forma de protección.
+                Con una cuerda amarras la lanza en tu espalda para no hacerte daño por accidente, con eso listo, vuelves a caminar por todo este lugar lleno de oro ignorando por completo, después de todo, tu propia existencia no tiene codicia.`
+            },
+            
+            optionB: {
+                text: `Dejar`,
+                postD:`Dejas de lado la lanza que has levantado, hasta ahora no has tenido la necesidad de usar algún tipo de arma, por lo que es innecesario para ti llevártela.
+                Caminas por el lugar ignorando todo lo que te rodea, tratando de llegar a la salida de este sitio que ya ha comenzado a agobiarte, después de todo de qué sirve todo si no puedes utilizarlo.`
+            },
+            finalText:`Ingresas en la cueva que te llevará al siguiente círculo.`
+        }
+    ]
+    
+}
+
+
